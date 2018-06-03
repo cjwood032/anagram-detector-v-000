f885728905b66f32582edc3f85883cc948166ce6
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-03 16:23:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 # Your code goes here!
+require 'pry'
 class Anagram
   def initialize(word)
     @word=word
@@ -9,6 +10,7 @@ class Anagram
     match_arr=[]
     while i < word_arr.length
       if @word.split("").sort==word_arr[i].split("").sort
+        binding.pry
         match_arr << word_arr[i]
       end
       i+=1
