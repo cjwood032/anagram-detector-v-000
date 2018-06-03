9df9513a4e33e9327465e7cd0144ba1c5511b1ce
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-06-03 16:18:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require pry
+class Anagram
+  def initialize(word)
+    @word=word
+  end
+  def match(words)
+    word_arr=%w(words)
+    i=0
+    while i < word_arr.length
+      if @word.split("").sort==word_arr[i].split("").sort
+        return word_arr[i]
+      end
+      i+=1
+    end
+  end
+end
